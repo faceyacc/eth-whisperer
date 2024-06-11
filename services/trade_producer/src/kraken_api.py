48099ae988e247d85b20b6afed748e98e474eb96
faceyacc <justfacey@gmail.com>
2024-06-11 05:08:21 -0400

    created trade producer

---
+++ services/trade_producer/src/kraken_api.py
@@ -0,0 +1,64 @@
+from typing import List, Dict
+from websocket import create_connection
+import json
+
+class KrakenWebsocketTradeAPI:
+    URL = 'wss://ws.kraken.com/v2'
+
+    def __init__(self, product_id) -> None:
+        self.product_id = product_id
+
+        # Establish connection to Kraken Websocket.
+        self._ws = create_connection(url=self.URL)
+        print("connection to Kraken successful!")
+
+        # Subscribe to trades.
+        self._subscribe(product_id=product_id)
+
+
+
+
+    def _subscribe(self, product_id) -> None:
+        """
+        Established an connection to Kraken Websocket API and subscribes to
+        to trades given product_id.
+
+        Args:
+            product_id (str): trade pair ticker.
+
+        Returns:
+            None
+        """
+
+        # Subscribe to Kraken trades
+        print("subscribing to trades...")
+        msg = {
+            "method": "subscribe",
+            "params": {
+                "channel": "trade",
+                "symbol": [
+                    product_id
+                ],
+                "snapshot": False
+            }
+        }
+
+        self._ws.send(json.dumps(msg))
+        print("subscription worked!")
+
+
+        # Dumping first two messages from Kraken.
+        _ = self._ws.recv()
+        _ = self._ws.recv()
+
+
+
+    def get_trades(self) -> List[Dict]:
+        msg = self._ws.recv()
+
+        if 'heartbeat' in str(msg):
+            return []
+
+        msg = json.loads(msg)
+
+        return msg['data']
---
+++ services/trade_producer/src/main.py
@@ -0,0 +1,50 @@
+from quixstreams import Application
+from src.kraken_api import KrakenWebsocketTradeAPI
+from typing import List, Dict
+
+
+def produce_trades(
+        kafka_broker_address: str,
+        kafka_topic_name: str) -> None:
+    """
+    Reads trades from Kraken websocket API and saves them to Kafka topic.
+
+    Args:
+        kafka_broker_address (str): The address of the Kafka broker.
+        kafka_topic_name (str): The name of the Kafka topic.
+
+    Returns:
+        None
+    """
+    app = Application(broker_address=kafka_broker_address)
+
+    # The topic to save trades
+    topic = app.topic(name=kafka_topic_name, value_serializer="json")
+
+    # Create kraken api instance.
+    kraken_api = KrakenWebsocketTradeAPI(product_id='BTC/USD')
+
+    # Create a Producer instance
+    with app.get_producer() as producer:
+        while True:
+            trades: List[Dict] = kraken_api.get_trades()
+            for trade in trades:
+                message = topic.serialize(key=trade["symbol"], value=trade)
+
+                producer.produce(
+                    topic=topic.name,
+                    value=message.value,
+                    key=message.key
+                )
+
+                print("success!")
+
+            from time import sleep
+            sleep(1)
+
+
+
+if __name__ == "__main__":
+    produce_trades(
+        kafka_broker_address="localhost:19092",
+        kafka_topic_name="trade")
