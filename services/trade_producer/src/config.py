c2ae872448960eb31386069e2b0b0057213966e1
faceyacc <justfacey@gmail.com>
2024-06-14 05:52:52 -0400

    Refactored code to us env vars.

--- services/trade_producer/src/config.py
+++ services/trade_producer/src/config.py
@@ -1,3 +1,8 @@
-kafka_broker_address = 'redpanda-0:9092'
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+kafka_broker_address = os.environ['KAFKA_BROKER_ADDRESS']
 kafka_topic_name = 'trade'
 product_id = 'BTC/USD'
