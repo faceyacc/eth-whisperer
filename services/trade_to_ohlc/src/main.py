b71ea79953c15587fe3ee03482fea9e1113230f2
faceyacc <justfacey@gmail.com>
2024-06-17 14:26:30 -0400

    Containerized trade_to_ohlc service.

--- services/trade_to_ohlc/src/main.py
+++ services/trade_to_ohlc/src/main.py
@@ -1,20 +1,21 @@
 from quixstreams import Application
 from loguru import logger
 from src.ohlc_config import config
-from quixstreams import message_context
 from datetime import timedelta
+
 
 def init_ohlc_candle(value: dict) -> dict:
     """
     Initalize the OHLC candle with first trade
     """
     return {
-        "open":value["price"],
-        "high":value["price"],
+        "open": value["price"],
+        "high": value["price"],
         "low": value["price"],
         "close": value["price"],
         "product_id": value["product_id"],
     }
+
 
 def update_ohlc_candle(ohlc_candle: dict, trade: dict) -> dict:
     """
@@ -27,18 +28,19 @@ def update_ohlc_candle(ohlc_candle: dict, trade: dict) -> dict:
         dict: The updated candle.
     """
     return {
-        "open":ohlc_candle["open"],
+        "open": ohlc_candle["open"],
         "high": max(ohlc_candle["high"], trade["price"]),
         "low": min(ohlc_candle["low"], trade["price"]),
         "close": trade["price"],
         "product_id": trade["product_id"],
     }
 
+
 def trade_to_ohlc(
     kafka_input_topic: str,
     kafka_output_topic: str,
     kafka_broker_addr: str,
-    ohlc_window_seconds: int
+    ohlc_window_seconds: int,
 ) -> None:
     """
     Reads trades from Kafka input topic
@@ -59,34 +61,30 @@ def trade_to_ohlc(
     app = Application(
         broker_address=kafka_broker_addr,
         consumer_group="trade_to_ohlc",
-        auto_offset_reset='earliest',
-
+        auto_offset_reset="earliest",
     )
 
     # Define input and output topics
-    output_topic = app.topic(name=kafka_output_topic, value_serializer='json')
-    input_topic = app.topic(name=kafka_input_topic, value_serializer='json')
-
+    output_topic = app.topic(name=kafka_output_topic, value_serializer="json")
+    input_topic = app.topic(name=kafka_input_topic, value_serializer="json")
 
     # Create StreamingDataFrame to apply transformations
     sdf = app.dataframe(input_topic)
-
 
     # Apply transformation to incoming data
     sdf = (
         sdf.tumbling_window(duration_ms=timedelta(seconds=ohlc_window_seconds))
         .reduce(reducer=update_ohlc_candle, initializer=init_ohlc_candle)
         .final()
-
     )
 
     # Unpack keys to get values
     sdf["open"] = sdf["value"]["open"]
     sdf["high"] = sdf["value"]["high"]
     sdf["low"] = sdf["value"]["low"]
-    sdf['close'] = sdf['value']['close']
+    sdf["close"] = sdf["value"]["close"]
     sdf["product_id"] = sdf["value"]["product_id"]
-    sdf['timestamp'] = sdf["end"]
+    sdf["timestamp"] = sdf["end"]
 
     # Extract crucial keys to give to Redpanda
     sdf = sdf[["timestamp", "open", "high", "low", "close", "product_id"]]
@@ -99,10 +97,11 @@ def trade_to_ohlc(
     # Run pipeline
     app.run(sdf)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     trade_to_ohlc(
-        kafka_output_topic= config.kafka_output_topic,
-        kafka_input_topic = config.kafka_input_topic,
-        kafka_broker_addr= config.kafka_broker_addr,
-        ohlc_window_seconds= config.ohlc_window_seconds,
+        kafka_output_topic=config.kafka_output_topic,
+        kafka_input_topic=config.kafka_input_topic,
+        kafka_broker_addr=config.kafka_broker_addr,
+        ohlc_window_seconds=config.ohlc_window_seconds,
     )
--- services/trade_to_ohlc/src/ohlc_config.py
+++ services/trade_to_ohlc/src/ohlc_config.py
@@ -4,10 +4,12 @@ from pydantic_settings import BaseSettings
 
 load_dotenv(find_dotenv())
 
+
 class Config(BaseSettings):
-    kafka_input_topic: str = 'trade'
-    kafka_output_topic: str = 'ohlc'
-    kafka_broker_addr: str = os.environ['KAFKA_BROKER_ADDRESS']
-    ohlc_window_seconds: int = int(os.environ['OHLC_WINDOW_SECONDS'])
+    kafka_input_topic: str = "trade"
+    kafka_output_topic: str = "ohlc"
+    kafka_broker_addr: str = os.environ["KAFKA_BROKER_ADDRESS"]
+    ohlc_window_seconds: int = int(os.environ["OHLC_WINDOW_SECONDS"])
+
 
 config = Config()
